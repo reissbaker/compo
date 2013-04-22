f89728efba6e0e239033173a2ef2212d65cad12a
Matt Baker <matthew.reiss.baker@gmail.com>
2013-04-22 01:42:58 -0700

    Remove unnecessary arithmetic.

--- demo/components/node-keyboard-controller.js
+++ demo/components/node-keyboard-controller.js
@@ -4,7 +4,7 @@
   var Component = seine.Component,
       keyboard = demo.keyboard;
 
-  var SPEED = 5;
+  var SPEED = 5 / 1000;
 
   var NodeKeyboardController = Component.extend(function(node) {
     Component.call(this);
@@ -14,10 +14,10 @@
   NodeKeyboardController.prototype.update = function(delta) {
     var node = this.node;
 
-    if(keyboard.down(keyboard.key.LEFT)) node.x -= SPEED * delta / 1000;
-    if(keyboard.down(keyboard.key.RIGHT)) node.x += SPEED * delta / 1000;
-    if(keyboard.down(keyboard.key.UP)) node.y -= SPEED * delta / 1000;
-    if(keyboard.down(keyboard.key.DOWN)) node.y += SPEED * delta / 1000;
+    if(keyboard.down(keyboard.key.LEFT)) node.x -= SPEED * delta;
+    if(keyboard.down(keyboard.key.RIGHT)) node.x += SPEED * delta;
+    if(keyboard.down(keyboard.key.UP)) node.y -= SPEED * delta;
+    if(keyboard.down(keyboard.key.DOWN)) node.y += SPEED * delta;
   };
 
   exports.NodeKeyboardController = NodeKeyboardController;
