0e7c05f66abd4ec59e949147e5932e8f5ae2577e
Matt Baker <matthew.reiss.baker@gmail.com>
2013-04-22 01:12:24 -0700

    Fun with one-liners.

--- demo/components/position-logger.js
+++ demo/components/position-logger.js
@@ -10,13 +10,12 @@
   var PositionLogger = Component.extend();
 
   PositionLogger.prototype.update = function(delta, x, y, z) {
-    if(modifier() && pressed()) {
-      console.log('position: (' + x + ', ' + y + ', ' + z + ')');
-    }
+    if(pressed()) console.log('position: (' + x + ', ' + y + ', ' + z + ')');
   };
 
+  function pressed() { return modifier() && key(); }
   function modifier() { return keyboard.down(MOD); }
-  function pressed() { return keyboard.pressed(KEY); }
+  function key() { return keyboard.pressed(KEY); }
 
   exports.PositionLogger = PositionLogger;
 
