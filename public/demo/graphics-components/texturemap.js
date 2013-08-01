fdee3899be6aa188bea7ccac1b0fc7e3b3df7876
Matt Baker <matthew.reiss.baker@gmail.com>
2013-08-01 02:35:32 -0700

    Add note about scoring algorithms for tex cache

--- public/demo/graphics-components/texturemap.js
+++ public/demo/graphics-components/texturemap.js
@@ -9,6 +9,12 @@
   // * If no, rebind to least most recently use texture
   // * Implement lru texture as a linked list duh
   // * Slots in tex array point to { id, iterator } tuples
+  //
+  // Note: use a scoring algorithm, NOT strict LRU. Every time something is
+  // used, it gets its score bumped. The list is the top 32 scoring textures: to
+  // get into the list, you need to beat the score of the lowest one. Add some
+  // sort of decay to the algorithm so that previously-hot but now cold textures
+  // get ejected relatively quickly.
   function TextureMap() {
   }
 
