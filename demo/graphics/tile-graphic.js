dabdfbd5f0467e74bdbaa945150e97dfd9b528bd
Matt Baker <matthew.reiss.baker@gmail.com>
2014-06-29 19:02:15 -0700

    [Cleanup] Nuke dead code

--- demo/graphics/tile-graphic.js
+++ demo/graphics/tile-graphic.js
@@ -20,24 +20,4 @@ TileGraphic.prototype.sprites = function() {
   return [this.frame.sprite];
 };
 
-function updateLocation(graphic, position, direction, offset) {
-  var gPos = graphic.sprite.position,
-      gScale = graphic.sprite.scale;
-
-  gPos.x = Math.round(position.x + offset.x);
-  gPos.y = Math.round(position.y + offset.y);
-
-  gScale.x = 1 * direction.x;
-  gScale.y = 1 * direction.y;
-
-  /*
-   * Flipping will result in the sprite appearing to jump (flips on the 0,
-   * rather than mid-sprite), so subtract the sprite's size from its position
-   * if it's flipped.
-   */
-
-  if(gScale.x < 0) gPos.x -= graphic.sprite.width;
-  if(gScale.y < 0) gPos.y -= graphic.sprite.height;
-}
-
 module.exports = TileGraphic;
