bf4fb322e61cbe4d300d6903e9eca3d0d2bfcabb
Matt Baker <matthew.reiss.baker@gmail.com>
2014-10-26 23:50:57 -0700

    Doc comments

--- typescript/core/database.ts
+++ typescript/core/database.ts
@@ -14,16 +14,44 @@ class Database {
 
   private _tables: Table<any>[] = [];
 
+
+  /*
+   * Table lifecycle
+   * ---------------------------------------------------------------------------
+   */
+
+  /*
+   * Creates a new Table<T> in the database.
+   */
+
   table<T extends Component>(): Table<T> {
     var table = new Table<T>();
     this._tables.push(table);
     return table;
   }
 
+
+  /*
+   * Removes a table from the database.
+   */
+
   drop<T extends Component>(table: Table<T>): Table<T> {
     util.remove(this._tables, table);
     return table;
   }
+
+
+  /*
+   * Entity lifecycle
+   * ---------------------------------------------------------------------------
+   */
+
+  /*
+   * Creates an entity with an optional parent.
+   *
+   * If given a parent, the database will ensure that the child is destroyed
+   * if the parent ever is.
+   */
 
   entity(parent: Entity = null): Entity {
     var id = this._guid++,
@@ -38,6 +66,13 @@ class Database {
 
     return entity;
   }
+
+
+  /*
+   * Destroys an entity.
+   *
+   * If the entity has any descendants, the descendants will also be destroyed.
+   */
 
   destroy(entity: Entity): Entity {
     if(!this._alive[entity.id]) return;
@@ -58,11 +93,27 @@ class Database {
     return entity;
   }
 
+
+  /*
+   * Cleanup methods
+   * ---------------------------------------------------------------------------
+   */
+
+  /*
+   * Compacts all the tables. This should be called periodically to ensure the
+   * tables don't become too sparse, which would hurt performance.
+   */
+
   compact() {
     util.each(this._tables, (table) => {
       table.compact();
     });
   }
+
+
+  /*
+   * Resets the database's state.
+   */
 
   reset() {
     this._alive = {};
@@ -76,13 +127,35 @@ class Database {
     this._tables = [];
   }
 
+
+  /*
+   * Accessors
+   * ---------------------------------------------------------------------------
+   */
+
+  /*
+   * Returns all of the children of the given entity.
+   */
+
   getChildren(entity: Entity): Entity[] {
     return this._children[entity.id];
   }
 
+
+  /*
+   * Returns the parent of the given entity, if one exists.
+   */
+
   getParent(entity: Entity): Entity {
     return this._parents[entity.id];
   }
+
+
+  /*
+   * Returns true if the given entity is live (has never been destroyed).
+   *
+   * Returns false otherwise.
+   */
 
   isAlive(entity: Entity): boolean {
     return !!this._alive[entity.id];
