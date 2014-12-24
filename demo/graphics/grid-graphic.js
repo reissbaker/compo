c6c237dce21858c67c6e7596b5ed875ac3a3142b
Matt Baker <matthew.reiss.baker@gmail.com>
2014-12-23 20:07:16 -0800

    [graphics] Correctly handle multi-row GridGraphics

--- demo/graphics/grid-graphic.js
+++ demo/graphics/grid-graphic.js
@@ -6,22 +6,32 @@ var compo = require('compo'),
     Point = require('../data/point'),
     Rect = require('../data/rect');
 
-var GridGraphic = compo.extend(Graphic, function(position, url, matrix, tileSize) {
-  var r, c, curr;
+var GridGraphic = compo.extend(Graphic, function(args) {
+  var position = args.position,
+      url = args.url,
+      matrix = args.matrix,
+      tileSize = args.tileSize,
+      spacing = args.spacing || 0,
+      crop = args.crop,
+      cols = (crop.width + spacing) / (tileSize.x + spacing),
+      rows = (crop.height + spacing) / (tileSize.y + spacing);
+
+  var x, y, r, c, curr, col, row;
 
   this.frames = [];
   for(r = 0; r < matrix.numRows; r++) {
     for(c = 0; c < matrix.numCols; c++) {
       curr = matrix.get(r, c);
+      col = curr % cols;
+      row = (curr / cols) | 0;
+
+      x = col * tileSize.x + col * spacing;
+      y = row * tileSize.y + row * spacing;
+
       if(curr >= 0) {
         this.frames.push(new Frame(position, url, {
           offset: new Point(c * tileSize.x, r * tileSize.y),
-          crop: new Rect(
-            curr * tileSize.x,
-            0,
-            tileSize.x,
-            tileSize.y
-          )
+          crop: new Rect(x, y, tileSize.x, tileSize.y)
         }));
       }
     }
